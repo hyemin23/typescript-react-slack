42b7f12f7f0036a3fa110ddb99ade03648b91251
hyemin <hm_stom@naver.com>
2021-08-18 23:52:05 +0900

    #Menu style & function setting

---
+++ alecture/components/Menu/index.tsx
@@ -0,0 +1,30 @@
+import React, { CSSProperties, FC } from 'react';
+import { useCallback } from 'react';
+import { CloseModalButton, CreateMenu } from './styles';
+
+interface Props {
+  show: boolean;
+  onCloseModal: () => void;
+  style: CSSProperties;
+  closeButton?: boolean;
+}
+const Menu: FC<Props> = ({ children, style, show, onCloseModal, closeButton }) => {
+  const stopPropagation = useCallback((e) => {
+    e.stopPropagation();
+  }, []);
+  return (
+    <CreateMenu onClick={onCloseModal}>
+      <div style={style} onClick={stopPropagation}>
+        {closeButton && <CloseModalButton onClick={onCloseModal}>&times;</CloseModalButton>}
+      </div>
+      {children}
+    </CreateMenu>
+  );
+};
+
+// Props들의 default(기본 값) 설정 시
+Menu.defaultProps = {
+  closeButton: true,
+};
+
+export default Menu;
--- alecture/layouts/Workspace/index.tsx
+++ alecture/layouts/Workspace/index.tsx
@@ -1,6 +1,6 @@
 import fetcher from '@utils/fetcher';
 import axios from 'axios';
-import React, { FC, useCallback } from 'react';
+import React, { FC, useCallback, useState } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import useSWR, { mutate } from 'swr';
 import {
@@ -17,6 +17,7 @@ import {
 import gravatar from 'gravatar';
 import DirectMessage from '@pages/DirectMessage';
 import loadable from '@loadable/component';
+import Menu from '@components/Menu';
 
 const Channel = loadable(() => import('@pages/Channel'));
 
@@ -25,6 +26,8 @@ const Channel = loadable(() => import('@pages/Channel'));
 // children을 사용하지 않는 컴포넌트 type : VFC
 // type error는 FC로 가져오면 됨
 const Workspace: FC = ({ children }) => {
+  const [showUserMenu, setShowUserMenu] = useState(false);
+
   const {
     data: userData,
     error: loginError,
@@ -49,6 +52,10 @@ const Workspace: FC = ({ children }) => {
       });
   }, []);
 
+  const onClickUserProfile = useCallback(() => {
+    setShowUserMenu((prev) => !prev);
+  }, []);
+
   // 로그아웃이 되는 순간 data는 false가 되므로
   if (!userData) {
     console.log(userData);
@@ -58,7 +65,14 @@ const Workspace: FC = ({ children }) => {
     <div>
       <Header>
         <RightMenu>
-          <ProfileImg src={gravatar.url(userData.nickname, { s: '28px', d: 'retro' })} />
+          <span onClick={onClickUserProfile}>
+            <ProfileImg src={gravatar.url(userData.nickname, { s: '28px', d: 'retro' })} />
+            {showUserMenu && (
+              <Menu style={{ right: 0, top: 38 }} show={showUserMenu} onCloseModal={onClickUserProfile}>
+                프로필메뉴
+              </Menu>
+            )}
+          </span>
         </RightMenu>
       </Header>
       <button onClick={onLogOut}>로그아웃</button>
