b7dce8e96ba9f58d8b6d1f29ea4d6b48ca0e3ca4
hyemin <hm_stom@naver.com>
2021-08-22 14:31:25 +0900

    #Modal

--- alecture/components/CreateChannelModal/index.tsx
+++ alecture/components/CreateChannelModal/index.tsx
@@ -1,17 +1,65 @@
 import Modal from '@components/Modal';
 import useInput from '@hooks/useInput';
 import { Button, Input, Label } from '@pages/SignUp/styles';
-import React, { useCallback, useState, VFC } from 'react';
+import { IChannel, IUser } from '@typings/db';
+import fetcher from '@utils/fetcher';
+import axios from 'axios';
+import React, { useCallback, VFC } from 'react';
+import { useParams } from 'react-router';
+import { toast } from 'react-toastify';
+import useSWR from 'swr';
 
-interface Iprops {
+interface Props {
   show: boolean;
   onCloseModal: () => void;
   setShowCreateChannelModal: (flag: boolean) => void;
 }
-const CreateChannelModal: VFC<Iprops> = ({ show, onCloseModal, setShowCreateChannelModal }) => {
+const CreateChannelModal: VFC<Props> = ({ show, onCloseModal, setShowCreateChannelModal }) => {
   const [newChannel, onChangeNewChannel, setNewChannel] = useInput('');
+  // router에서 넘어오는 params들을 기억
+  const { workspace, channel } = useParams<{ workspace: string; channel: string }>();
+  const {
+    data: userData,
+    error,
+    revalidate,
+  } = useSWR<IUser | false>('/api/users', fetcher, {
+    dedupingInterval: 2000, // 2초
+  });
 
-  const onCreateChannel = useCallback((e) => {}, []);
+  // mutate는 전송을 막기 위해 사용 기존에 갖고 있던 data를 넣음.
+  // revalidate는 서버에 요청을 한번 더 해서 정보를 다시 가져옴
+  const {
+    data: channelData,
+    mutate,
+    revalidate: revalidateChannel,
+  } = useSWR<IChannel[]>(userData ? `/api/workspaces/${workspace}/channels` : null, fetcher);
+
+  const onCreateChannel = useCallback(
+    (e) => {
+      e.preventDefault();
+      axios
+        .post(
+          `/api/workspaces/${workspace}/channels`,
+          {
+            name: newChannel,
+          },
+          {
+            withCredentials: true,
+          },
+        )
+        .then((response) => {
+          setShowCreateChannelModal(false);
+          revalidateChannel();
+          setNewChannel('');
+        })
+        .catch((error) => {
+          console.dir(error);
+          toast.error(error.response?.data, { position: 'bottom-center' });
+        });
+    },
+    [newChannel],
+  );
+
   return (
     <Modal show={show} onCloseModal={onCloseModal}>
       <form onSubmit={onCreateChannel}>
