b6c403f081b7a59d9b331e27407414960a7c788e
hyemin <hm_stom@naver.com>
2021-08-22 14:32:26 +0900

    #Common Chat Box divide

--- alecture/pages/Channel/index.tsx
+++ alecture/pages/Channel/index.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import ChatBox from '@components/ChatBox';
+import ChatList from '@components/ChatList';
+import useInput from '@hooks/useInput';
+import React, { useCallback } from 'react';
 import { Container, Header } from './style';
 
 const Channel = () => {
+  const [chat, onChangeChat, setChat] = useInput('');
+  const onSubmitForm = useCallback((e) => {
+    e.preventDefault();
+    console.log('부모 input 서브밋');
+    setChat('');
+  }, []);
+
   return (
     <Container>
       <Header>채널</Header>
+      <ChatList />
+      {/* 재사용되는것들은 props로 전달 */}
+      <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} placeholder="asd" />
     </Container>
   );
 };
--- alecture/pages/DirectMessage/index.tsx
+++ alecture/pages/DirectMessage/index.tsx
@@ -1,8 +1,44 @@
-import Workspace from '@layouts/Workspace';
-import React from 'react';
+import { Header } from '@pages/SignUp/styles';
+import React, { useCallback } from 'react';
+import gravatar from 'gravatar';
+import useSWR from 'swr';
+import { useParams } from 'react-router-dom';
+import fetcher from '@utils/fetcher';
+import ChatList from '@components/ChatList';
+import ChatBox from '@components/ChatBox';
+import { Container } from '@pages/Channel/style';
+import useInput from '@hooks/useInput';
 
 const DirectMessage = () => {
-  return <div>DM 페이지</div>;
+  const { workspace, id } = useParams<{ workspace: string; id: string }>();
+
+  // 상대방 데이터
+  const { data: userData } = useSWR(`/api/workspaces/${workspace}/members/${id}`, fetcher);
+
+  // 내 정보
+  const { data: myData } = useSWR(`/api/users`, fetcher);
+
+  //
+  const [chat, onChangeChat] = useInput('');
+
+  const onSubmitForm = useCallback((e) => {
+    e.preventDefault();
+    console.log('submit');
+  }, []);
+  // 로딩중이거나 error
+  if (!userData || !myData) {
+    return null;
+  }
+  return (
+    <Container>
+      <Header>
+        <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.nickname} />
+        <span>{userData.nickname}</span>
+      </Header>
+      <ChatList />
+      <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} placeholder="" />
+    </Container>
+  );
 };
 
 export default DirectMessage;
