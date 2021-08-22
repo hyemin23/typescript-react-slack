1a030dbe0a668e37f25caf4533e081b161dc3398
hyemin <hm_stom@naver.com>
2021-08-22 14:30:53 +0900

    #Chat & ChatBox & ChatLst & DMList

---
+++ alecture/components/ChatBox/index.tsx
@@ -0,0 +1,69 @@
+import { ChatArea, Form, MentionsTextarea, SendButton, Toolbox, EachMention } from '@components/ChatBox/styles';
+import autosize from 'autosize';
+import React, { useCallback, useEffect, useRef, VFC } from 'react';
+interface Props {
+  chat: string;
+  placeholder: string;
+  onSubmitForm: (e: any) => void;
+  onChangeChat: (e: any) => void;
+}
+const ChatBox: VFC<Props> = ({ chat, onChangeChat, onSubmitForm, placeholder }) => {
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+  useEffect(() => {
+    if (textareaRef.current) {
+      autosize(textareaRef.current);
+    }
+  }, [textareaRef.current]);
+
+  const onKeydownChat = useCallback((e) => {
+    // 태그를 직접 접근하고 싶을 때
+    // Ref의 타입은 typescript에서만 적용해주면 됨.
+
+    if (e.key == 'Enter') {
+      // 줄 바꿈 처리 shift + enter 가 아닌 경우만
+      if (!e.shiftKey) {
+        e.preventDefault();
+        onSubmitForm(e);
+      }
+    }
+  }, []);
+
+  // 공통 compoentns인 chat box는 dm 채팅, channel의 채팅에 같이 사용 중. 따라서 여기서는 props로 올려줘야함 부모한테
+  // 여기서 구체적인 작업을 하면 다른곳에서 재사용을 못함 즉, 나를 사용하는 쪽으로 구체적인 작업을 하게끔 올려줘야함.
+  // const opSubmitForm = useCallback(() => {}, []);
+
+  // 단 공통되는 data는 hook으로 가져와도 됨 (단, 재사용되는데 서로 다른 data는 밖으로 빼주면 됨.)
+
+  return (
+    <ChatArea>
+      <Form onSubmit={onSubmitForm}>
+        <MentionsTextarea
+          id="editor-chat"
+          value={chat}
+          onChange={onChangeChat}
+          onKeyDown={onKeydownChat}
+          placeholder={placeholder}
+          ref={textareaRef}
+        ></MentionsTextarea>
+        <Toolbox>
+          <SendButton
+            className={
+              'c-button-unstyled c-icon_button c-icon_button--light c-icon_button--size_medium c-texty_input__button c-texty_input__button--send' +
+              (chat?.trim() ? '' : ' c-texty_input__button--disabled')
+            }
+            data-qa="texty_send_button"
+            aria-label="Send message"
+            data-sk="tooltip_parent"
+            type="submit"
+            disabled={!chat?.trim()}
+          >
+            <i className="c-icon c-icon--paperplane-filled" aria-hidden="true" />
+          </SendButton>
+        </Toolbox>
+      </Form>
+    </ChatArea>
+  );
+};
+
+export default ChatBox;
---
+++ alecture/components/ChatList/index.tsx
@@ -0,0 +1,13 @@
+import Chat from '@components/Chat';
+import { ChatZone, Section, StickyHeader } from '@components/ChatList/styles';
+import React, { FC, RefObject, useCallback } from 'react';
+
+const ChatList = () => {
+  return (
+    <ChatZone>
+      <Section>section</Section>
+    </ChatZone>
+  );
+};
+
+export default ChatList;
